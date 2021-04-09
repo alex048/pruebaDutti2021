7597264256d94e42978ba68b5db62076d2d43445
Alex Barrientos <abarrientos@confirma.pe>
2021-04-09 11:01:03 -0500

    se restructura el proyecto (se crea la carpeta auth para mover los components login y register)/(se crea la carperta shared para mover el component principal ) y en la carpeta components dejando los page-one, page-two, ships)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { PrincipalModule } from './components/principal/principal.module';
+import { PrincipalModule } from './shared/principal/principal.module';
 
 // Components
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { PrincipalComponent } from './components/principal/principal.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PrincipalComponent } from './shared/principal/principal.component';
 
 
 @NgModule({
